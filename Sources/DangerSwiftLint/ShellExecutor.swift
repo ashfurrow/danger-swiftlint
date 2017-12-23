d0299956487daac10eca2b912670a7388bf2f1ae
Ash Furrow <ash@ashfurrow.com>
2017-12-23 18:05:23 -0500

    Fixed script invocation.

--- Sources/DangerSwiftLint/ShellExecutor.swift
+++ Sources/DangerSwiftLint/ShellExecutor.swift
@@ -1,15 +1,17 @@
 import Foundation
 
 internal class ShellExecutor {
-    func execute(_ launchPath: String, arguments: String...) -> String {
+    func execute(_ command: String, arguments: String...) -> String {
+        let script = "\(command) \(arguments.joined(separator: " "))"
+        print("Executing \(script)")
+
         var env = ProcessInfo.processInfo.environment
         let task = Process()
         task.launchPath = env["SHELL"]
-        task.arguments = ["-l", "-c", launchPath] + arguments
+        task.arguments = ["-l", "-c", script]
 
         let pipe = Pipe()
         task.standardOutput = pipe
-        print("Executing \(launchPath) \(arguments.joined(separator: " "))")
         task.launch()
         task.waitUntilExit()
 
