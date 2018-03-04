ab961aee0bb9743aa31d5940532933a6c61526c6
Łukasz Mróz <lukasz.mroz@droidsonroids.pl>
2018-03-04 21:13:01 +0100

    Add inline support

--- Sources/DangerSwiftLint/DangerSwiftLint.swift
+++ Sources/DangerSwiftLint/DangerSwiftLint.swift
@@ -8,10 +8,10 @@ public struct SwiftLint {
     /// This is the main entry point for linting Swift in PRs using Danger-Swift.
     /// Call this function anywhere from within your Dangerfile.swift.
     @discardableResult
-    public static func lint(directory: String? = nil, configFile: String? = nil) -> [Violation] {
+    public static func lint(inline: Bool = false, directory: String? = nil, configFile: String? = nil) -> [Violation] {
         // First, for debugging purposes, print the working directory.
         print("Working directory: \(shellExecutor.execute("pwd"))")
-        return self.lint(danger: danger, shellExecutor: shellExecutor, directory: directory, configFile: configFile)
+        return self.lint(danger: danger, shellExecutor: shellExecutor, inline: inline, directory: directory, configFile: configFile)
     }
 }
 
@@ -20,10 +20,13 @@ internal extension SwiftLint {
     static func lint(
         danger: DangerDSL,
         shellExecutor: ShellExecutor,
+        inline: Bool = false,
         directory: String? = nil,
         configFile: String? = nil,
-        markdown: (String) -> Void = markdown,
-        fail: (String) -> Void = fail) -> [Violation] {
+        markdownAction: (String) -> Void = markdown,
+        failAction: (String) -> Void = fail,
+        failInlineAction: (String, String, Int) -> Void = fail,
+        warnInlineAction: (String, String, Int) -> Void = warn) -> [Violation] {
         // Gathers modified+created files, invokes SwiftLint on each, and posts collected errors+warnings to Danger.
 
         var files = danger.git.createdFiles + danger.git.modifiedFiles
@@ -40,20 +43,31 @@ internal extension SwiftLint {
             do {
                 return try decoder.decode([Violation].self, from: outputJSON.data(using: String.Encoding.utf8)!)
             } catch let error {
-                fail("Error deserializing SwiftLint JSON response (\(outputJSON)): \(error)")
+                failAction("Error deserializing SwiftLint JSON response (\(outputJSON)): \(error)")
                 return []
             }
         }
 
         if !violations.isEmpty {
-            var markdownMessage = """
-            ### SwiftLint found issues
+            if inline {
+                violations.forEach { violation in
+                    switch violation.severity {
+                    case .error:
+                        failInlineAction(violation.reason, violation.file, violation.line)
+                    case .warning:
+                        warnInlineAction(violation.reason, violation.file, violation.line)
+                    }
+                }
+            } else {
+                var markdownMessage = """
+                ### SwiftLint found issues
 
-            | Severity | File | Reason |
-            | -------- | ---- | ------ |\n
-            """
-            markdownMessage += violations.map { $0.toMarkdown() }.joined(separator: "\n")
-            markdown(markdownMessage)
+                | Severity | File | Reason |
+                | -------- | ---- | ------ |\n
+                """
+                markdownMessage += violations.map { $0.toMarkdown() }.joined(separator: "\n")
+                markdownAction(markdownMessage)
+            }
         }
 
         return violations
